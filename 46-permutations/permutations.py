98ad09f8e048b4c3c77412583abbadef84abb38e
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-06-27 17:19:08 -0700

    Time: 38 ms (74.89%) | Memory: 16.7 MB (24.60%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,19 +1,32 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        maps = [0] * len(nums)
-        ans = []
-        arr = []
-
-        def perm(arr,maps):
-            if len(arr) == len(nums):
-                ans.append(arr[:])
-                return 
-            for i in range(len(nums)):
-                if (maps[i] == False):
-                    arr.append(nums[i])
-                    maps[i] = True
-                    perm(arr,maps)
-                    arr.remove(nums[i])
-                    maps[i] = False
-        perm(arr,maps)
+        ans =[]
+        ind = 0
+        def perm(ind):
+            if ind == len(nums):
+                ans.append(nums[:])
+                return
+            for i in range(ind,len(nums)):
+                nums[ind],nums[i] = nums[i], nums[ind]
+                perm(ind+1)
+                nums[ind],nums[i] = nums[i], nums[ind]
+        perm(0)
         return ans
+
+        # maps = [0] * len(nums)
+        # ans = []
+        # arr = []
+
+        # def perm(arr,maps):
+        #     if len(arr) == len(nums):
+        #         ans.append(arr[:])
+        #         return 
+        #     for i in range(len(nums)):
+        #         if (maps[i] == False):
+        #             arr.append(nums[i])
+        #             maps[i] = True
+        #             perm(arr,maps)
+        #             arr.remove(nums[i])
+        #             maps[i] = False
+        # perm(arr,maps)
+        # return ans
