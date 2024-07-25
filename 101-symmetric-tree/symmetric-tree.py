9ddf8f302435339a44a8cc32ccfa87a44ef03ade
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-07-25 00:30:09 -0700

    Time: 45 ms (8.40%) | Memory: 16.5 MB (81.39%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -9,10 +9,8 @@ class Solution:
         def dfs(node1,node2):
             if not node1 and not node2:
                 return True
-            # One of the nodes is None, tree is not symmetric
             if not node1 or not node2:
                 return False
-            # Nodes have different values, tree is not symmetric
             if node1.val != node2.val:
                 return False
             return dfs(node1.left,node2.right) and dfs(node1.right,node2.left)
