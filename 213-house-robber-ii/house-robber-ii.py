8a820fe65ac277d33e2c701b6fbf21113f3a036e
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-05 19:18:35 -0700

    Time: 39 ms (36.76%) | Memory: 16.6 MB (6.95%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,11 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def robber(nums):
-            rob1 , rob2 = 0, 0
+        def dp(nums):
+            memo = [0] * len(nums)
 
-            for i in nums:
-                tmp = max(i+rob1,rob2)
-                rob1 = rob2
-                rob2 = tmp
-            return rob2
-        return max(nums[0],robber(nums[1:]),robber(nums[:-1]))
+            memo[0] = nums[0]
+
+            for i in range(1, len(nums)):
+                memo[i] = max(memo[i - 1], memo[i - 2] + nums[i])
+
+            return memo[-1]
+
+        if len(nums) == 1:
+            return nums[0]
+        
+        return max(dp(nums[1:]), dp(nums[:-1]))
+        # def robber(nums):
+        #     rob1 , rob2 = 0, 0
+
+        #     for i in nums:
+        #         tmp = max(i+rob1,rob2)
+        #         rob1 = rob2
+        #         rob2 = tmp
+        #     return rob2
+        # return max(nums[0],robber(nums[1:]),robber(nums[:-1]))
