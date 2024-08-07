31bf35b4d1549e534454a4956101cc3482a509e6
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-06 20:07:49 -0700

    Time: 25 ms (98.04%) | Memory: 16.6 MB (25.02%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,18 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-        def helper(n):
-            if n in memo:
-                return memo[n]
-            if n ==0:
-                return nums[n]
-            if n<0:
-                return 0
-            pick = nums[n] + helper(n-2)
-            notpick = 0 + helper(n-1)
-            memo[n] = max(pick,notpick)
-            return memo[n]
-        return helper(len(nums)-1)
+        prev,prev2 = 0,0
+        for n in nums:
+            temp = max(prev+n, prev2)
+            prev = prev2
+            prev2 = temp
+        return prev2
+        # memo = {}
+        # def helper(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     if n ==0:
+        #         return nums[n]
+        #     if n<0:
+        #         return 0
+        #     pick = nums[n] + helper(n-2)
+        #     notpick = 0 + helper(n-1)
+        #     memo[n] = max(pick,notpick)
+        #     return memo[n]
+        # return helper(len(nums)-1)
         # dp = len(nums)*[0]
         # dp[0] = nums[0]
         # if len(nums)>1:
