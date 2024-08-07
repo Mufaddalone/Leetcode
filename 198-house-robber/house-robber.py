2c43ef5864ae524670d4f56f4f33268b850243f0
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-06 19:45:06 -0700

    Time: 32 ms (79.70%) | Memory: 16.5 MB (25.02%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        memo = {}
+        def helper(n):
+            if n in memo:
+                return memo[n]
+            if n ==0:
+                return nums[n]
+            if n<0:
+                return 0
+            pick = nums[n] + helper(n-2)
+            notpick = 0 + helper(n-1)
+            memo[n] = max(pick,notpick)
+            return memo[n]
+        return helper(len(nums)-1)
         # dp = len(nums)*[0]
         # dp[0] = nums[0]
         # if len(nums)>1:
@@ -10,10 +23,10 @@ class Solution:
         # for i in range(2,len(nums)):
         #     dp[i] = nums[i] + dp[i-2]
         # return max(dp[-1],dp[-2])
-        rob1, rob2 = 0, 0
+        # rob1, rob2 = 0, 0
 
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        # for n in nums:
+        #     temp = max(n + rob1, rob2)
+        #     rob1 = rob2
+        #     rob2 = temp
+        # return rob2
