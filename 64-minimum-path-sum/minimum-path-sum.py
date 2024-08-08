10e72af930055e4ffb65f6d32ef928b0a509fb1f
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-07 21:57:30 -0700

    Time: 87 ms (45.30%) | Memory: 18.4 MB (28.05%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,32 +1,36 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        dp = [0] * n
+        # m = len(grid)
+        # n = len(grid[0])
+        # dp = [0]*n
 
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    dp[j] = grid[i][j]
-                elif i == 0:
-                    dp[j] = dp[j-1] + grid[i][j]
-                elif j == 0:
-                    dp[j] = dp[j] + grid[i][j]
-                else:
-                    dp[j] = min(dp[j], dp[j-1]) + grid[i][j]
-
-        return dp[-1]
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i ==0 and j==0:
+        #             dp[j] = grid[i][j]
+        #         elif i == 0:
+        #             dp[j] = dp[j-1] + grid[i][j]
+        #         elif j == 0:
+        #             dp[j] = dp[j] + grid[i][j]
+        #         else:
+        #             dp[j] = min(dp[j],dp[j-1])+ grid[i][j]
+        # return dp[-1]
 
         # m=len(grid)
         # n=len(grid[0])
+        # prev = [0]*n
         # for i in range(m):
         #     for j in range(n):
-        #         if i>0 and j>0:
-        #             grid[i][j] += min(grid[i-1][j],grid[i][j-1])
-        #         elif i>0:
-        #             grid[i][0]+=grid[i-1][0]
-        #         elif j>0:
-        #             grid[0][j]+=grid[0][j-1]
-        # return grid[m-1][n-1]
+        #         if i == 0 and j == 0:
+        #             dp[i][j] = grid[i][j]
+        #         elif i == 0:
+        #             dp[i][j] = dp[i][j-1] + grid[i][j]
+        #         elif j == 0:
+        #             dp[i][j] = dp[i-1][j] + grid[i][j]
+        #         else:
+        #             dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        # return dp[m-1][n-1]
 
         # dp = [[-1 for i in range(len(grid[0]))]for j in range(len(grid))]
         # def helper(i,j,dp):
@@ -41,3 +45,25 @@ class Solution:
         #     dp[i][j] = min(up,left)
         #     return dp[i][j]
         # return helper(len(grid)-1,len(grid[0])-1,dp)
+
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1 for i in range(len(grid[0]))]for j in range(len(grid))]
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    dp[i][j] = grid[i][j]
+                else:
+                    up = grid[i][j]
+                    if i>0:
+                        up+= dp[i-1][j]
+                    else:
+                        up += int(1e9)
+                    left = grid[i][j]
+                    if j>0:
+                        left+= dp[i][j-1]
+                    else:
+                        left += int(1e9)
+                    dp[i][j] = min(left,up)
+        return dp[m-1][n-1]
+                
