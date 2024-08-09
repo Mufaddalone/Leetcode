8317aae94e635d99d923c0e5bf7e8b5f5ab0ae8f
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-09 08:54:57 -0700

    Time: 120 ms (83.89%) | Memory: 17.4 MB (67.57%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -2,32 +2,35 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         m = len(matrix[0])
-        dp =[[-1 for i in range(m)]for j in range(n)]
+        # dp =[[-1 for i in range(m)]for j in range(n)]
+        prev = [0]*m
+        curr = [0]*m
         for i in range(m):
-            dp[0][i] = matrix[0][i]
+            prev[i] = matrix[0][i]
         for i in range(1,n):
             for j in range(m):
-                up = matrix[i][j] + dp[i - 1][j]
+                up = matrix[i][j] + prev[j]
             
                 # Handle left diagonal
                 left_diagonal = matrix[i][j]
                 if j - 1 >= 0:
-                    left_diagonal += dp[i - 1][j - 1]
+                    left_diagonal += prev[j - 1]
                 else:
                     left_diagonal += int(1e9)  # A large negative value if out of bounds
                 
                 # Handle right diagonal
                 right_diagonal = matrix[i][j]
                 if j + 1 < m:
-                    right_diagonal += dp[i - 1][j + 1]
+                    right_diagonal += prev[j + 1]
                 else:
                     right_diagonal += int(1e9)  # A large negative value if out of bounds
                 
                 # Store the maximum of the three moves in dp
-                dp[i][j] = min(up, left_diagonal, right_diagonal)
-        maxi = dp[n-1][0]
+                curr[j] = min(up, left_diagonal, right_diagonal)
+            prev = curr[:]
+        maxi = prev[n-1]
         for j in range(m):
-            maxi = min(maxi,dp[n-1][j])
+            maxi = min(maxi,prev[j])
         return maxi
         # def f(i,j):
         #     if (j<0 or j>=m):
