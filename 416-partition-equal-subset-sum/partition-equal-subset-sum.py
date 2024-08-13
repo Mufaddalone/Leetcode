d807bf2d3cc132e758ffa7e7d479f93920534f42
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-13 00:41:58 -0700

    Time: 525 ms (65.28%) | Memory: 16.5 MB (99.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,20 +1,39 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        arr_sum = sum(nums)
-        if arr_sum % 2 != 0:
+        total_sum = sum(nums)
+    
+        # If total sum is odd, we cannot partition it into two equal subsets
+        if total_sum % 2 != 0:
             return False
-        sub_sum = arr_sum//2
+        
+        target = total_sum // 2
         n = len(nums)
-        dp = [[False for j in range(sub_sum+1)]for i in range(n)]
-        for i in range(n):
-            dp[i][0] = True
-        if nums[0] <= sub_sum:
-            dp[0][nums[0]] = True
-        for ind in range(1,n):
-            for target in range(1,sub_sum+1):
-                notake = dp[ind-1][target]
-                take = False
-                if nums[ind] <= target:
-                    take = dp[ind-1][target-nums[ind]]
-                dp[ind][target] = notake or take
-        return dp[n-1][sub_sum]
+        
+        # Initialize a DP array where dp[i] means if the sum i can be formed
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for num in nums:
+            # Update the DP array from right to left
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        
+        return dp[target]
+        # arr_sum = sum(nums)
+        # if arr_sum % 2 != 0:
+        #     return False
+        # sub_sum = arr_sum//2
+        # n = len(nums)
+        # dp = [[False for j in range(sub_sum+1)]for i in range(n)]
+        # for i in range(n):
+        #     dp[i][0] = True
+        # if nums[0] <= sub_sum:
+        #     dp[0][nums[0]] = True
+        # for ind in range(1,n):
+        #     for target in range(1,sub_sum+1):
+        #         notake = dp[ind-1][target]
+        #         take = False
+        #         if nums[ind] <= target:
+        #             take = dp[ind-1][target-nums[ind]]
+        #         dp[ind][target] = notake or take
+        # return dp[n-1][sub_sum]
