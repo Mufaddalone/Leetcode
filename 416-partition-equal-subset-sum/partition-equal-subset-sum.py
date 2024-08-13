8d18a6ef7718e7391b3a949b3dfaf4d44394c00a
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-13 00:40:45 -0700

    Time: 1716 ms (22.24%) | Memory: 32.9 MB (33.10%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,28 +1,20 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        array_sum = sum(nums)
-
-        # If array sum is odd, it cannot be partitioned into equal sum subsets.
-        if array_sum % 2 != 0:
+        arr_sum = sum(nums)
+        if arr_sum % 2 != 0:
             return False
-
-        # Calculate the subset sum.
-        subset_sum = array_sum // 2
-
-        # Create a lookup table and fill all entries with False.
-        dp = [[False for _ in range(len(nums) + 1)] for _ in range(subset_sum + 1)]
-
-        # Initialize the first row as True as each array has a subset whose sum is zero
-        for i in range(len(nums) + 1):
-            dp[0][i] = True
-
-        # Fill the lookup table in a bottom-up manner.
-        for i in range(1, subset_sum + 1):
-            for j in range(1, len(nums) + 1):
-                if nums[j - 1] > i:
-                    dp[i][j] = dp[i][j - 1]
-                else:
-                    dp[i][j] = dp[i - nums[j - 1]][j - 1] or dp[i][j - 1]
-
-        # Return the last index of the matrix, which is our answer
-        return dp[subset_sum][len(nums)]
+        sub_sum = arr_sum//2
+        n = len(nums)
+        dp = [[False for j in range(sub_sum+1)]for i in range(n)]
+        for i in range(n):
+            dp[i][0] = True
+        if nums[0] <= sub_sum:
+            dp[0][nums[0]] = True
+        for ind in range(1,n):
+            for target in range(1,sub_sum+1):
+                notake = dp[ind-1][target]
+                take = False
+                if nums[ind] <= target:
+                    take = dp[ind-1][target-nums[ind]]
+                dp[ind][target] = notake or take
+        return dp[n-1][sub_sum]
