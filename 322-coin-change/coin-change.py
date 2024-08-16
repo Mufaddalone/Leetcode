6e0a9c674bcc6947f56a90321f5c941bbfccd06b
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-16 16:43:25 -0700

    Time: 749 ms (73.21%) | Memory: 16.7 MB (94.05%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,20 +1,31 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [[-1 for j in range(amount+1)]for i in range(len(coins))]
-        def f(ind,target):
-            if ind ==0:
-                if target %coins[ind] == 0:
-                    return target//coins[ind]
-                else:
-                    return float("inf")
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            notpick = 0 + f(ind-1,target)
-            take = float('inf')
-            if coins[ind] <= target:
-                take = 1 + f(ind,target - coins[ind])
-            dp[ind][target] = min(take,notpick)
-            return dp[ind][target]
-        result = f(len(coins) - 1, amount)
+        dp = [math.inf] * (amount+1)
+        dp[0] = 0
+
+        for a in range(1,amount+1):
+            for coin in coins:
+                if coin <= a:
+                    dp[a] = min(dp[a],1+ dp[a-coin])
         
-        return result if result != float('inf') else -1
+        # If dp[amount] is still infinity, it means we can't form that amount with the given coins
+        
+        return dp[amount] if dp[amount] != math.inf else -1
+        # dp = [[-1 for j in range(amount+1)]for i in range(len(coins))]
+        # def f(ind,target):
+        #     if ind ==0:
+        #         if target %coins[ind] == 0:
+        #             return target//coins[ind]
+        #         else:
+        #             return float("inf")
+        #     if dp[ind][target]!=-1:
+        #         return dp[ind][target]
+        #     notpick = 0 + f(ind-1,target)
+        #     take = float('inf')
+        #     if coins[ind] <= target:
+        #         take = 1 + f(ind,target - coins[ind])
+        #     dp[ind][target] = min(take,notpick)
+        #     return dp[ind][target]
+        # result = f(len(coins) - 1, amount)
+        
+        # return result if result != float('inf') else -1
