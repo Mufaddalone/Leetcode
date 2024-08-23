1ed6b173a3ed7992e7554f707785488a9861f652
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-22 19:11:27 -0700

    Time: 1263 ms (10.47%) | Memory: 308.5 MB (12.17%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,14 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[-1 for i in range(len(text2)+1)]for j in range(len(text1)+1)]
+        # dp = [[-1 for i in range(len(text2)+1)]for j in range(len(text1)+1)]
+        memo = {}
         def f(ind1,ind2):
             if ind1<0 or ind2<0:
                 return 0
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
+            # if dp[ind1][ind2]!=-1:
+            #     return dp[ind1][ind2]
+            if (ind1,ind2) in memo:
+                return memo[(ind1,ind2)]
+            # if text1[ind1] == text2[ind2]:
+            #     dp[ind1][ind2] = 1 + f(ind1-1,ind2-1)
+            #     return dp[ind1][ind2]
             if text1[ind1] == text2[ind2]:
-                dp[ind1][ind2] = 1 + f(ind1-1,ind2-1)
-                return dp[ind1][ind2]
-            dp[ind1][ind2]= max(f(ind1-1,ind2),f(ind1,ind2-1))
-            return dp[ind1][ind2]
+                memo [(ind1,ind2)] = 1 + f(ind1-1,ind2-1)
+            # dp[ind1][ind2]= max(f(ind1-1,ind2),f(ind1,ind2-1))
+            # return dp[ind1][ind2]
+            else: memo[(ind1,ind2)]= max(f(ind1-1,ind2),f(ind1,ind2-1))
+            return memo[(ind1,ind2)]
         return f(len(text1)-1,len(text2)-1)
