f80bb5754c568b3b46fa36b231552aadd4b3e7c3
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-22 19:39:46 -0700

    Time: 455 ms (81.19%) | Memory: 42.5 MB (41.81%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,23 +1,20 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n = len(text1)
-        m = len(text2)
-        
-        # Create a DP table of size (n+1) x (m+1) initialized with -1
-        dp = [[-1 for j in range(m + 1)] for i in range(n + 1)]
+        n,m = len(text1),len(text2)
+        '''
+        Take smaller string 
+        and find most common occurences???
+        '''
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
 
-        for i in range(n+1):
-            dp[i][0] = 0
-        for j in range(m+1):
-            dp[0][j] = 0
-        
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1+ dp[i-1][j-1]
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        return dp[n][m]
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[0][0]
 
         # # dp = [[-1 for i in range(len(text2)+1)]for j in range(len(text1)+1)]
         # memo = {}
