972712446cd92be0a09275be38e2975bea233703
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-26 15:10:40 -0700

    Time: 514 ms (47.79%) | Memory: 42.5 MB (42.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -5,16 +5,23 @@ class Solution:
         Take smaller string 
         and find most common occurences???
         '''
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        dp = [[0 for j in range(m + 1)] for i in range(n + 1)]
 
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[n][m]
+        # for i in range(len(text1) - 1, -1, -1):
+        #     for j in range(len(text2) - 1, -1, -1):
+        #         if text1[i] == text2[j]:
+        #             dp[i][j] = 1 + dp[i + 1][j + 1]
+        #         else:
+        #             dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
 
-        return dp[0][0]
+        # return dp[0][0]
 
         # # dp = [[-1 for i in range(len(text2)+1)]for j in range(len(text1)+1)]
         # memo = {}
