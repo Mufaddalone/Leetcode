e945086fce034f11770de9f5b46dc642d8c0fff7
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-01 11:58:36 -0700

    Time: 982 ms (37.79%) | Memory: 30.2 MB (97.09%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,37 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[-1 for j in range(5)]for i in range(n)]
-        def prof(ind,tras):
-            if ind ==n:
-                return 0
-            if tras == 0:
-                return 0
-            if dp[ind][tras] != -1:
-                return dp[ind][tras]
+        # dp = [[0 for j in range(5)] for i in range(n + 1)]
+        curr = [0]*5
+        prev = [0]*5
 
-            if (tras%2 ==0):
-                profit = max(-prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
-            else:
-                profit = max(prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
+        for ind in range(n - 1, -1, -1):
+            for trans in range(3, -1, -1):
+                if trans % 2 == 0:  # Buy
+                    profit = max(-prices[ind] + prev[trans + 1], prev[trans])
+                else:  # Sell
+                    profit = max(prices[ind] + prev[trans + 1], prev[trans])
+                curr[trans] = profit
+            prev = curr
 
-            dp[ind][tras] = profit
-            return profit
-        return prof(0,4)
+        return curr[0]
+            
+        # def prof(ind,tras):
+        #     if ind ==n:
+        #         return 0
+        #     if tras == 0:
+        #         return 0
+        #     if dp[ind][tras] != -1:
+        #         return dp[ind][tras]
+
+        #     if (tras%2 ==0):
+        #         profit = max(-prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
+        #     else:
+        #         profit = max(prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
+
+        #     dp[ind][tras] = profit
+        #     return profit
+        # return prof(0,4)
         # n = len(prices)
         # dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
         
