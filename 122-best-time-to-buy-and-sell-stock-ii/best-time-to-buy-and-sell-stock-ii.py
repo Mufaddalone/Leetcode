3c437bc22b6ce8a0a90b6eb878a296e7897517dd
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-08-31 20:30:28 -0700

    Time: 54 ms (78.14%) | Memory: 17.9 MB (57.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp = [[-1 for i in range(2)]for j in range(n+1)]
-        dp[n][0] = dp[n][1] = 0
+        max_profit=0
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                max_profit+=prices[i]- prices[i-1]
+        return max_profit
+        # n = len(prices)
+        # dp = [[-1 for i in range(2)]for j in range(n+1)]
+        # dp[n][0] = dp[n][1] = 0
 
-        for ind in range(n-1,-1,-1):
-            for buy in range(2):
-                if dp[ind][buy] != -1:
-                    return dp[ind][buy]
-                if (buy):
-                    profit = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
-                else:
-                    profit = max(prices[ind]+dp[ind+1][1],dp[ind+1][0])
-                dp[ind][buy] = profit
-        return dp[ind][buy]
+        # for ind in range(n-1,-1,-1):
+        #     for buy in range(2):
+        #         if dp[ind][buy] != -1:
+        #             return dp[ind][buy]
+        #         if (buy):
+        #             profit = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
+        #         else:
+        #             profit = max(prices[ind]+dp[ind+1][1],dp[ind+1][0])
+        #         dp[ind][buy] = profit
+        # return dp[ind][buy]
 
         # n = len(prices)
         # dp = [[-1 for i in range(2)]for j in range(n)]
