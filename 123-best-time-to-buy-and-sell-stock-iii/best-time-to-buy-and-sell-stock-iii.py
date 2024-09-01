b78fc612f4dd3053183ec921cb5d9ca546116a72
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-01 11:21:01 -0700

    Time: 1606 ms (26.00%) | Memory: 66.5 MB (20.14%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,23 +1,41 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
-        
-        def prof(ind, buy, cap):
-            if ind == n or cap == 0:
+        dp = [[-1 for j in range(5)]for i in range(n)]
+        def prof(ind,tras):
+            if ind ==n:
                 return 0
-            
-            if dp[ind][buy][cap] != -1:
-                return dp[ind][buy][cap]
-            
-            if buy:
-                profit = max(-prices[ind] + prof(ind+1, 0, cap),
-                             prof(ind+1, 1, cap))
+            if tras == 0:
+                return 0
+            if dp[ind][tras] != -1:
+                return dp[ind][tras]
+
+            if (tras%2 ==0):
+                profit = max(-prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
             else:
-                profit = max(prices[ind] + prof(ind+1, 1, cap-1),
-                             prof(ind+1, 0, cap))
-            
-            dp[ind][buy][cap] = profit
+                profit = max(prices[ind]+prof(ind+1,tras-1),prof(ind+1,tras))
+
+            dp[ind][tras] = profit
             return profit
+        return prof(0,4)
+        # n = len(prices)
+        # dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n)]
         
-        return prof(0, 1, 2)
+        # def prof(ind, buy, cap):
+        #     if ind == n or cap == 0:
+        #         return 0
+            
+        #     if dp[ind][buy][cap] != -1:
+        #         return dp[ind][buy][cap]
+            
+        #     if buy:
+        #         profit = max(-prices[ind] + prof(ind+1, 0, cap),
+        #                      prof(ind+1, 1, cap))
+        #     else:
+        #         profit = max(prices[ind] + prof(ind+1, 1, cap-1),
+        #                      prof(ind+1, 0, cap))
+            
+        #     dp[ind][buy][cap] = profit
+        #     return profit
+        
+        # return prof(0, 1, 2)
