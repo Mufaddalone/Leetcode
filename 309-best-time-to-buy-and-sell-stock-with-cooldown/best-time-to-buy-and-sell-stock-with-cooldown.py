da6095a14881231975e2ef1a845082f19863bc00
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-01 15:55:55 -0700

    Time: 47 ms (44.03%) | Memory: 17.1 MB (65.75%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,10 +3,6 @@ class Solution:
         n = len(prices)
         dp = [[0 for j in range(2)]for i in range(n+2)]
         for ind in range(n-1,-1,-1):
-            for buy in range(2):
-                if (buy):
-                    profit = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
-                else:
-                    profit = max(prices[ind]+dp[ind+2][1],dp[ind+1][0])
-                dp[ind][buy] = profit
+            dp[ind][1] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
+            dp[ind][0] = max(prices[ind]+dp[ind+2][1],dp[ind+1][0])
         return dp[0][1]
