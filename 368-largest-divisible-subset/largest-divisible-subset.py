000a6aa67a507674d8618f47f47dafb348eaa408
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-05 15:34:41 -0700

    Time: 223 ms (55.38%) | Memory: 16.7 MB (84.12%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -0,0 +1,49 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        dp = [1] * len(nums)
+        hash = [0] * len(nums)
+        lastindex,maxlen = 0,1
+        for i in range(0,len(nums)):
+            hash[i] = i
+            for j in range(i):
+                if (nums[j]%nums[i]==0 and dp[j]+1>dp[i]):
+                    dp[i] = dp[j]+1
+                    hash[i] = j
+                if dp[i]>maxlen:
+                    maxlen=dp[i]
+                    lastindex = i
+        result = [nums[lastindex]]
+        while hash[lastindex] != lastindex:
+            lastindex = hash[lastindex]
+            result.append(lastindex)
+        return result
+
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        
+        nums.sort()
+        n = len(nums)
+        dp = [1] * n
+        hash = list(range(n))
+        max_len, max_index = 1, 0
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    hash[i] = j
+            if dp[i] > max_len:
+                max_len = dp[i]
+                max_index = i
+
+        result = []
+        while len(result) < max_len:
+            result.append(nums[max_index])
+            max_index = hash[max_index]
+
+        return result[::-1]
+        
