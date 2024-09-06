47474596710a8e62a1a9416242124f422152981c
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-06 14:32:40 -0700

    Time: 868 ms (21.63%) | Memory: 16.8 MB (91.38%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -1,35 +1,24 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        if not words:
-            return 0
-        
-        words.sort(key=len)
-        word_set = set(words)
-        dp = {}
-        
-        def is_predecessor(word1, word2):
+        def compare(word1, word2):
             if len(word2) - len(word1) != 1:
                 return False
             i = j = 0
-            while i < len(word1):
-                if (word1[i] == word2[j] and j < len(word2)):
-                    i+=1
-                    j+=1
-                else:
-                    i+=1
-            return (i == len(word1) and j == len(word2))
-            #     if word1[i] == word2[j]:
-            #         i += 1
-            #     j += 1
-            # return i == len(word1)
-        
-        max_chain = 1
-        for word in words:
-            dp[word] = 1
-            for i in range(len(word)):
-                predecessor = word[:i] + word[i+1:]
-                if predecessor in word_set:
-                    dp[word] = max(dp[word], dp[predecessor] + 1)
-            max_chain = max(max_chain, dp[word])
-        
-        return max_chain
+            while i < len(word1) and j < len(word2):
+                if word1[i] == word2[j]:
+                    i += 1
+                j += 1
+            return i == len(word1)
+
+        words.sort(key=len)
+        n = len(words)
+        dp = [1] * n
+        max_len = 1
+
+        for i in range(1, n):
+            for j in range(i):
+                if compare(words[j], words[i]) and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+            max_len = max(max_len, dp[i])
+
+        return max_len
