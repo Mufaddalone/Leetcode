5162a6f7eb58bbf50e1e073b337d053b38ca7fbd
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-10 16:15:48 -0700

    Time: 821 ms (53.94%) | Memory: 18 MB (59.28%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -5,16 +5,32 @@ class Solution:
         c = len(cuts)
         dp = [[-1] * c for _ in range(c)]
 
-        # Recursive function to find the minimum cost
-        def f(i, j):
-            # Base case
+        def cutting(i, j):
             if j - i <= 1:
                 return 0
-            
             if dp[i][j] != -1:
                 return dp[i][j]
             
-            dp[i][j] = min(cuts[j] - cuts[i] + f(i, k) + f(k, j) for k in range(i+1, j))
+            mini = float('inf')
+            for ind in range(i+1, j):
+                cut = cuts[j] - cuts[i] + cutting(i, ind) + cutting(ind, j)
+                mini = min(mini, cut)
+            
+            dp[i][j] = mini
             return dp[i][j]
 
-        return f(0, c-1)
+        return cutting(0, c - 1)
+
+        # # Recursive function to find the minimum cost
+        # def f(i, j):
+        #     # Base case
+        #     if j - i <= 1:
+        #         return 0
+            
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+            
+        #     dp[i][j] = min(cuts[j] - cuts[i] + f(i, k) + f(k, j) for k in range(i+1, j))
+        #     return dp[i][j]
+
+        # return f(0, c-1)
