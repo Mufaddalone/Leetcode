2278c6b237cfc77bd23b129983ccd13ee8945cca
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-11 12:40:37 -0700

    Time: 286 ms (65.98%) | Memory: 16.6 MB (58.08%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -1,20 +1,33 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        dp = [-1]*n
-        def part(ind):
-            if ind==n:
-                return 0
-            if dp[ind]!=-1:
-                return dp[ind]
+        dp = [0]*(n+1)
+        for ind in range(n,-1,-1):
             lens = 0
             maxans = 0
             maxi = float("-inf")
             for j in range(ind,min(n,ind+k)):
                 lens+=1
                 maxi = max(maxi,arr[j])
-                sum = (lens*maxi) + part(j+1)
+                sum = (lens*maxi) + dp[j+1]
                 maxans = max(sum,maxans)
                 dp[ind] = maxans
-            return dp[ind]
-        return part(0)
+        return dp[0]
+        # n = len(arr)
+        # dp = [-1]*n
+        # def part(ind):
+        #     if ind==n:
+        #         return 0
+        #     if dp[ind]!=-1:
+        #         return dp[ind]
+        #     lens = 0
+        #     maxans = 0
+        #     maxi = float("-inf")
+        #     for j in range(ind,min(n,ind+k)):
+        #         lens+=1
+        #         maxi = max(maxi,arr[j])
+        #         sum = (lens*maxi) + part(j+1)
+        #         maxans = max(sum,maxans)
+        #         dp[ind] = maxans
+        #     return dp[ind]
+        # return part(0)
