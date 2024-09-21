ec1bd8ec3fea986e8cd1f05f18eab476d421b9d2
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-20 22:10:55 -0700

    Time: 104 ms (13.93%) | Memory: 18.3 MB (54.22%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,25 +1,50 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = {i :[] for i in range(numCourses)}
+        # 1. Initialize data structures
+        adj_list = defaultdict(list)
+        indegrees = [0] * numCourses
 
-        for crs , pre in prerequisites:
-            adj[crs].append(pre)
+        # 2. Build the graph
+        for a, b in prerequisites:
+            indegrees[a] += 1
+            adj_list[b].append(a)
 
-        visited = set()
+        # 3. Initialize queue with courses having no prerequisites
+        q = deque()
+        for i, c in enumerate(indegrees):
+            if not c:
+                q.append(i)
 
-        def dfs(crs):
-            if crs in visited:
-                return False
-            if adj[crs] == []:
-                return True
-            visited.add(crs)
-            for pre in adj[crs]:
-                if not dfs(pre): return False
-            visited.remove(crs)
-            adj[crs] = []
-            return True
-        for crs in range(numCourses):
-            if not dfs(crs):return False
-        return True
+        # 4. Perform topological sorting
+        while q:
+            c = q.popleft()
+            for n in adj_list[c]:
+                indegrees[n] -= 1
+                if indegrees[n] == 0:
+                    q.append(n)
+
+        # 5. Check if all courses can be taken
+        return True if sum(indegrees) == 0 else False
+        # adj = {i :[] for i in range(numCourses)}
+
+        # for crs , pre in prerequisites:
+        #     adj[crs].append(pre)
+
+        # visited = set()
+
+        # def dfs(crs):
+        #     if crs in visited:
+        #         return False
+        #     if adj[crs] == []:
+        #         return True
+        #     visited.add(crs)
+        #     for pre in adj[crs]:
+        #         if not dfs(pre): return False
+        #     visited.remove(crs)
+        #     adj[crs] = []
+        #     return True
+        # for crs in range(numCourses):
+        #     if not dfs(crs):return False
+        # return True
 
         
