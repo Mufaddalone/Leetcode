bf14260b6d94845e76c2da0fd49b30ec97d59c03
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-20 22:15:23 -0700

    Time: 106 ms (12.05%) | Memory: 18.3 MB (81.55%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,30 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # 1. Initialize data structures
-        adj_list = defaultdict(list)
-        indegrees = [0] * numCourses
+        adj = defaultdict(list)
+        indegree = [0] * numCourses
+        for crs ,pre in prerequisites:
+            indegree[crs] +=1
+            adj[pre].append(crs)
 
-        # 2. Build the graph
-        for a, b in prerequisites:
-            indegrees[a] += 1
-            adj_list[b].append(a)
-
-        # 3. Initialize queue with courses having no prerequisites
         q = deque()
-        for i, c in enumerate(indegrees):
+        for i,c in enumerate(indegree):
             if not c:
                 q.append(i)
-
-        # 4. Perform topological sorting
+        
         while q:
-            c = q.popleft()
-            for n in adj_list[c]:
-                indegrees[n] -= 1
-                if indegrees[n] == 0:
-                    q.append(n)
-
-        # 5. Check if all courses can be taken
-        return True if sum(indegrees) == 0 else False
+            crs = q.popleft()
+            for pre in adj[crs]:
+                indegree[pre] -= 1
+                if indegree[pre]==0:
+                    q.append(pre)
+        return True if sum(indegree)==0 else False
         # adj = {i :[] for i in range(numCourses)}
 
         # for crs , pre in prerequisites:
