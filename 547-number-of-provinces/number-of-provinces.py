08143c020bf8cd43359904cc7ec309656318b178
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-09-23 20:55:17 -0700

    Time: 186 ms (49.25%) | Memory: 17.8 MB (19.66%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,16 +1,16 @@
 class Solution:
     def findCircleNum(self, a: List[List[int]]) -> int:
-        visited = [False] * len(a)
+        visited = [0] * len(a)
         pro = 0
 
         def dfs(i):
             visited[i] = True
             for j in range(len(a)):
-                if a[i][j]==1 and visited[j] == False:
+                if a[i][j] == 1 and visited[j] == False:
                     dfs(j)
-                
+
         for i in range(len(a)):
-            if not visited[i]:
+            if visited[i] == False:
                 pro+=1
                 dfs(i)
         return pro
