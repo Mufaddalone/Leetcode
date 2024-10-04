b0909972c1350df0370e6f875e65d723403a4498
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-10-04 14:22:17 -0700

    Time: 408 ms (89.20%) | Memory: 19.7 MB (48.70%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,25 +1,33 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n = len(mat[0])  # Number of columns
+        m = len(mat)     # Number of rows
         q = deque()
-        visited = set()
-        m = len(mat)
-        n = len(mat[0])
-        dist = [[0 for j in range(n)] for i in range(m)]
+        dist = [[-1] * n for _ in range(m)]  # Initialize with -1 indicating unvisited
+        
+        # Enqueue all the '0' cells and set their distances to 0
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
-                    q.append([i,j,0])
-                    dist[i][j] = 0
-                    visited.add((i,j))
-        dri = [[0,1],[1,0],[-1,0],[0,-1]]
+                    q.append((i, j, 0))  # Append (i, j, distance=0)
+                    dist[i][j] = 0  # Distance to itself is 0
+        
+        # Directions for moving up, down, left, and right
+        dri = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        
+        # BFS traversal
         while q:
-            r,c,dis = q.popleft()
-            dist[r][c] = dis
-            for dr,dc in dri:
-                nr,nc = dr+r,c+dc
-                if nr>=m or nc>=n or nr<0 or nc<0 or (nr,nc) in visited:
-                    continue 
-                q.append([nr,nc,dis+1])
-                visited.add((nr, nc))
+            r, c, dis = q.popleft()
+            
+            # Explore the 4 neighboring cells
+            for dr, dc in dri:
+                nr, nc = r + dr, c + dc
+                
+                # Check if the new position is within bounds and hasn't been visited
+                if 0 <= nr < m and 0 <= nc < n and dist[nr][nc] == -1:
+                    dist[nr][nc] = dis + 1  # Update distance
+                    q.append((nr, nc, dis + 1))  # Enqueue the neighbor
+        
         return dist
-
+            
+            
