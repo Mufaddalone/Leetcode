f51e763809fdaacde5ac57bcc83d696b2e363d0a
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-10-10 11:34:03 -0700

    Time: 568 ms (17.30%) | Memory: 24.2 MB (60.77%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,26 +1,48 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        m = len(graph)
-        safe = []
-        visit = [0]*(m)
-        path = [0]*(m)
-        check = [0]*(m)
-        def dfs(i):
-            visit[i] = 1
-            path[i] = 1
+        adj = defaultdict(list)
+        for i in range(len(graph)):
             for j in graph[i]:
-                if not visit[j]:
-                    if dfs(j):
-                        return True
-                elif path[j]:
-                    return True
-            path[i] = 0  
-            check[i] = 1
-            return False
-        for i in range(m):
-            if visit[i] == 0:
-                dfs(i)
-        for i in range(m):
-            if check[i] ==1:
-                safe.append(i)
-        return safe
+                adj[j].append(i)        
+        outdegree = [0] * len(graph)
+        q = deque()
+        result = []
+        for i in range(len(graph)):
+            for j in graph[i]:
+                outdegree[i]+=1
+        for i in range(len(outdegree)):
+            if  outdegree[i]==0:
+                q.append(i)
+        while q:
+            node = q.popleft()
+            result.append(node)
+            for i in adj[node]:
+                outdegree[i]-=1
+                if outdegree[i] == 0:
+                    q.append(i)
+        return sorted(result)
+
+        # m = len(graph)
+        # safe = []
+        # visit = [0]*(m)
+        # path = [0]*(m)
+        # check = [0]*(m)
+        # def dfs(i):
+        #     visit[i] = 1
+        #     path[i] = 1
+        #     for j in graph[i]:
+        #         if not visit[j]:
+        #             if dfs(j):
+        #                 return True
+        #         elif path[j]:
+        #             return True
+        #     path[i] = 0  
+        #     check[i] = 1
+        #     return False
+        # for i in range(m):
+        #     if visit[i] == 0:
+        #         dfs(i)
+        # for i in range(m):
+        #     if check[i] ==1:
+        #         safe.append(i)
+        # return safe
