46761ccb35d59cfb497dea6db26961c7f5e7b783
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-10-10 10:36:27 -0700

    Time: 93 ms (41.67%) | Memory: 18.3 MB (82.95%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,28 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj = defaultdict(list)
+        # indegree = [0] * numCourses
         for crs,pre in prerequisites:
             adj[crs].append(pre)
-        visit = [0]*(numCourses)
-        path = [0]*(numCourses)
-        result=[]
-        def dfs(i):
-            visit[i] = 1
-            path[i] = 1
+        q = deque()
+        indegree= [0]*numCourses
+        for i in adj:
             for j in adj[i]:
-                if not visit[j]:
-                    if dfs(j):
-                        return True
-                elif path[j]:
-                    return True
-            path[i] = 0  # Remove from current DFS path
-            visit[i] = 2  # Mark as fully processed (visited)
-            result.append(i)  # Add course to result in topological order
-            return False
+                indegree[j] +=1
         for i in range(numCourses):
-            if visit[i] == 0:
-                if dfs(i):
-                    return []
-        return result
+            if indegree[i] == 0:
+                q.append(i)
+        topo = []
+        while q:
+            node = q.popleft()
+            topo.append(node)
+            for i in adj[node]:
+                indegree[i] -=1
+                if indegree[i] == 0:
+                    q.append(i)
+        if len(topo)==numCourses:
+            return topo[::-1] 
+        else:
+            return []
                 
                 
