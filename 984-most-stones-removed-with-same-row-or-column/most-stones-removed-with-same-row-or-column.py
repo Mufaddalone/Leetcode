0850a1ffd862bf9d9582719fbd3cc365c9436682
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-10-28 20:20:01 -0700

    Time: 19 ms (66.04%) | Memory: 18.2 MB (19.09%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -19,26 +19,54 @@ class Solution:
             else:
                 size[root_v] += size[root_u]
                 parent[root_u] = root_v
-        max_row = 0
+
+        max_row= 0
         max_col = 0
         for i in stones:
             max_row = max(max_row,i[0])
             max_col = max(max_col,i[1])
-        stoneNodes = {}
         n = max_row + max_col +1
         parent = [i for i in range(n+1)]
         size = [0]*(n+1)
+        stonedge = {}
+        count = 0
         for i in stones:
             row = i[0]
-            col = i[1] + max_row +1
+            col = i[1] + max_row + 1
+            stonedge[row] = 1
+            stonedge[col] = 1
             union(row,col)
-            stoneNodes[row] = 1
-            stoneNodes[col] = 1
-        print(stoneNodes,parent)
-        count = 0
-        for i in stoneNodes:
+        for i in stonedge:
             if (find(i) == i):
                 count+=1
-        return len(stones) - count
+        return len(stones)-count
+
+
+        
+
+
+
+
+
+        # max_row = 0
+        # max_col = 0
+        # for i in stones:
+        #     max_row = max(max_row,i[0])
+        #     max_col = max(max_col,i[1])
+        # stoneNodes = {}
+        # n = max_row + max_col +1
+        # parent = [i for i in range(n+1)]
+        # size = [0]*(n+1)
+        # for i in stones:
+        #     row = i[0]
+        #     col = i[1] + max_row +1
+        #     union(row,col)
+        #     stoneNodes[row] = 1
+        #     stoneNodes[col] = 1
+        # count = 0
+        # for i in stoneNodes:
+        #     if (find(i) == i):
+        #         count+=1
+        # return len(stones) - count
 
 
