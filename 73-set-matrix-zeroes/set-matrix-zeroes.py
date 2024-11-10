048ebcf964e4b7ff72b0005ea0a516c72a2bafbb
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-11-10 14:10:26 -0800

    Time: 3 ms (70.06%) | Memory: 17.4 MB (87.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,20 +3,50 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m,n = len(matrix), len(matrix[0])
-        a = set()
-        b = set()
-        for i in range(m):
-            for j in range(n):
-                if(matrix[i][j]==0):
-                    a.add(i)
-                    b.add(j)
+        m, n = len(matrix), len(matrix[0])
+        rowzero = False
+
+        # First pass: mark zeros on the first row and first column
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowzero = True
+
+        # Second pass: use marks to set elements to zero
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        # Set the first column to zero if needed
+        if matrix[0][0] == 0:
+            for r in range(m):
+                matrix[r][0] = 0
+
+        # Set the first row to zero if `rowzero` is True
+        if rowzero:
+            for c in range(n):
+                matrix[0][c] = 0
+
+
+
+        # a = set()
+        # b = set()
+        # for i in range(m):
+        #     for j in range(n):
+        #         if(matrix[i][j]==0):
+        #             a.add(i)
+        #             b.add(j)
         
-        for i in a:
-            matrix[i] = [0]*n
+        # for i in a:
+        #     matrix[i] = [0]*n
         
-        for j in b:
-            for i in range(m): matrix[i][j]=0
+        # for j in b:
+        #     for i in range(m): matrix[i][j]=0
         # n = len(matrix[0])
         # m = len(matrix)
         # row = [0]*m
