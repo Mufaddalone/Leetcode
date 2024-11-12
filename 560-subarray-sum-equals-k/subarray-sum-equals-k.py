641ac75995fa09db15e12c4ac5cadb394d36ad25
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-11-12 10:39:22 -0800

    Time: 34 ms (52.97%) | Memory: 19.5 MB (30.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,28 +1,43 @@
 class Solution:
     def subarraySum(self, a: List[int], k: int) -> int:
-        n = len(a) # size of the array.
-
-        preSumMap = {}
-        Sum = 0
+        d = {}
+        sums = 0
         count = 0
+        n = len(a)
         for i in range(n):
-            Sum += a[i]
-
-            if Sum == k:
+            sums+= a[i]
+            if sums ==k:
                 count+=1
-
-            rem = Sum - k
-
-            if rem in preSumMap:
-                count+= preSumMap[rem]
-
-            if Sum in preSumMap:
-                preSumMap[Sum] += 1
+            rem = sums-k
+            if rem in d:
+                count += d[rem]
+            if sums in d:
+                d[sums] +=1
             else:
-                preSumMap[Sum] = 1
-
-
+                d[sums] = 1
         return count
+        # n = len(a) # size of the array.
+
+        # preSumMap = {}
+        # Sum = 0
+        # count = 0
+        # for i in range(n):
+        #     Sum += a[i]
+
+        #     if Sum == k:
+        #         count+=1
+
+        #     rem = Sum - k
+
+        #     if rem in preSumMap:
+        #         count+= preSumMap[rem]
+
+        #     if Sum in preSumMap:
+        #         preSumMap[Sum] += 1
+        #     else:
+        #         preSumMap[Sum] = 1
+
+        # return count
         # count = 0
         # n = len(nums)
         # for i in range(n):
