2f65878578b7d10d7a521df00534b9118788a369
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-11-12 06:47:07 -0800

    Time: 0 ms (100.00%) | Memory: 16.7 MB (11.26%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -7,6 +7,7 @@ class Solution:
         for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
         for i in range(n):
             matrix[i].reverse()
         
