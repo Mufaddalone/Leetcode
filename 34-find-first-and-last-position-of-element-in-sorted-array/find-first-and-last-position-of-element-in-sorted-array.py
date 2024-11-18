8ef23179d1e0113d700f17de7b2f350f07e3ca58
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-11-18 09:17:10 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.99%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,30 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findFirst(nums, target):
-            l, r = 0, len(nums) - 1
+        def firstele():
+            l,r = 0, len(nums)-1
             first = -1
-            while l <= r:
-                mid = (l + r) // 2
+            while l<=r:
+                mid = (r+l)//2
                 if nums[mid] == target:
                     first = mid
-                    r = mid - 1  # Continue searching to the left
+                    r = mid-1
                 elif nums[mid] < target:
-                    l = mid + 1
+                    l = mid+1
                 else:
-                    r = mid - 1
+                    r=mid-1
             return first
-
-        def findLast(nums, target):
-            l, r = 0, len(nums) - 1
-            last = -1
-            while l <= r:
-                mid = (l + r) // 2
+        def lastele():
+            l,r = 0, len(nums)-1
+            last =-1
+            while l<=r:
+                mid = (r+l)//2
                 if nums[mid] == target:
                     last = mid
-                    l = mid + 1  # Continue searching to the right
+                    l = mid+1
                 elif nums[mid] < target:
-                    l = mid + 1
+                    l = mid+1
                 else:
-                    r = mid - 1
+                    r=mid -1
             return last
-
-        first = findFirst(nums, target)
-        last = findLast(nums, target)
-        return [first, last]
+        
+        return [firstele(),lastele()]
