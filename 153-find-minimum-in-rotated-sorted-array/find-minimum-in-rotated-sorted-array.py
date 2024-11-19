1429765ec805ca379cf7465120d91cea349fd872
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-11-19 07:16:36 -0800

    Time: 0 ms (100.00%) | Memory: 16.9 MB (31.26%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,73 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l,r= 0,len(nums)-1
+        res = nums[0]
+        while l<=r:
+            if nums[l] < nums[r]:
+                res= min(res,nums[l])
+                break 
+            mid =(l+r)//2
+            res =min(res,nums[mid])
+            if nums[mid]>= nums[l]:
+                l =mid+1
+            else:
+                r =mid-1
+        return res
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[l]<nums[r]:
+        #         r =mid-1
+        #         res= min(res,nums[mid])
+        #     else:
+        #         l = mid+1
+        #         res = min (res,nums[mid])
+        # return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = nums[0]
+        # l,r = 0, len(nums)-1
+        # while l<=r:
+        #     if nums[l] < nums[r]:
+        #         res = min(res,nums[l])
+        #         break
+
+        #     mid = (l+r)//2
+        #     res = min(nums[mid],res)
+        #     if nums[mid] >= nums[l]:
+        #         l = mid+1
+        #     else:
+        #         r = mid-1
+        # return res
+
+
+            # for i in range(len(nums)-1):
+            #     if nums[i] > nums[i+1]:
+            #         return nums[i+1]
+            # return nums[0]
+            # low , high = 0, len(nums)-1
+            # while low<high:
+            #     mid = low + (high-low)//2
+            #     if nums[mid] > nums[high]:
+            #         low = mid + 1
+            #     else:
+            #         high = mid
+            # return nums[low]
+
+        
