c02b3f0b8fa485d69b4354dcde4babd5c59d0a0c
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-01-15 08:58:42 -0800

    Time: 179 ms (64.24%) | Memory: 32.8 MB (52.81%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -5,14 +5,38 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        a = []
-        while temp:
-            a.append(temp.val)
-            temp = temp.next
-        a.sort()
-        temp = head
-        for i in range(len(a)):
-            temp.val = a[i]
-            temp = temp.next
-        return head
+        def mid(head):
+            slow =head
+            fast =head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        def merge(list1,list2):
+            dummyNode = ListNode(-1)
+            temp = dummyNode
+            while list1  and list2 :
+                if list1.val <= list2.val:
+                    temp.next = list1
+                    list1 = list1.next
+                else:
+                    temp.next = list2
+                    list2 = list2.next
+                temp = temp.next 
+            if list1 is not None:
+                temp.next = list1
+            else:
+                temp.next = list2
+            return dummyNode.next
+        def mergesort(head):
+            if head is None or head.next is None:
+                return head
+            middle = mid(head)
+            lefthead, righthead = head,middle.next
+            middle.next = None
+            ls = mergesort(lefthead)
+            rs = mergesort(righthead)
+            return merge(ls,rs)
+        return mergesort(head)
+
