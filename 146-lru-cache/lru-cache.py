5407dfe4f428753b6833d259d5e828c72cfaf879
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-01-23 09:21:33 -0800

    Time: 140 ms (57.77%) | Memory: 78.6 MB (22.06%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,26 +1,26 @@
-class Node:
+class Node():
     def __init__(self,key,val):
         self.key,self.val = key,val
         self.prev = self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
+        self.left,self.right = Node(0,0),Node(0,0)
+        self.left.next,self.right.prev = self.right,self.left
+        
 
-        self.right, self.left = Node(0,0), Node(0,0)
-        self.left.next,self.right.prev=self.right,self.left
-    
     def remove(self,node):
-        prev,nxt = node.prev,node.next
+        prev, nxt = node.prev,node.next
         prev.next,nxt.prev = nxt,prev
-    
+
     def insert(self,node):
         prev,nxt = self.right.prev,self.right
-        prev.next=nxt.prev = node
-        node.next = nxt
-        node.prev =prev
-
+        prev.next,nxt.prev = node,node
+        node.prev = prev
+        node.next =nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -28,7 +28,7 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-        
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
@@ -39,6 +39,7 @@ class LRUCache:
             self.remove(lru)
             del self.cache[lru.key]
 
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
