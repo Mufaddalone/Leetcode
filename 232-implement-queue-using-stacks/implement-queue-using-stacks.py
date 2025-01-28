410de9348fe05eb9815108ba8d6f58aeaef54920
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-01-28 12:06:40 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.82%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -3,7 +3,6 @@ class MyQueue:
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
-        
 
     def push(self, x: int) -> None:
         while self.stack1:
@@ -11,43 +10,16 @@ class MyQueue:
         self.stack1.append(x)
         while self.stack2:
             self.stack1.append(self.stack2.pop())
-
-            
+        
 
     def pop(self) -> int:
         return self.stack1.pop()
-        
 
     def peek(self) -> int:
         return self.stack1[-1]
-        
 
     def empty(self) -> bool:
-        if len(self.stack1)<= 0:
-            return True
-        else:
-            return False
-
-    # def __init__(self):
-    #     self.q=[]
-
-    # def push(self, x: int) -> None:
-    #     self.q.append(x)
-    #     for i in range(len(self.q)-1):
-    #         self.q.append(self.q.pop(0))
-
-    # def pop(self) -> int:
-    #     return self.q.pop(len(self.q)-1)
-
-    # def peek(self) -> int:
-    #     return self.q[len(self.q)-1]
-
-    # def empty(self) -> bool:
-    #     if len(self.q)==0:
-    #         return True
-    #     return False
-
-        
+        return len(self.stack1)<= 0
 
 
 # Your MyQueue object will be instantiated and called as such:
