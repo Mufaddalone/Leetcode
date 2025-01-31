22a594be88b8345e7e1e95b45a7635643fe1294b
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-01-31 12:15:41 -0800

    Time: 54 ms (91.64%) | Memory: 21.6 MB (66.47%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,15 +1,12 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        stack = [] # keep index for the latest smaller values
-        res = [0] * len(arr)
-
+        stack = []
+        res = [0]*len(arr)
         for i in range(len(arr)):
-            while stack and arr[stack[-1]] > arr[i]:
+            while stack and arr[i]<= arr[stack[-1]]:
                 stack.pop()
-
             j = stack[-1] if stack else -1
-            res[i] = res[j] + (i - j) * arr[i]
+            res[i] = res[j] + (i-j)*arr[i]
 
             stack.append(i)
-        
         return sum(res) % (10**9+7)
