b321aa3afe7896eacd1708c11346a38983f8df6d
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-07 08:49:25 -0800

    Time: 30 ms (65.13%) | Memory: 18.6 MB (88.14%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
-        d = {}
-        count = 0
-        sums = 0
-        for i in range(len(nums)):
-            sums += nums[i]
-            if sums ==k:
-                count+=1
-            rem = sums - k
-            if rem in d:
-                count += d[rem]
-            if sums in d:
-                d[sums] += 1
-            else:
-                d[sums] = 1
-        return count
-            
+        def notgoal(nums,k):
+            l = 0 
+            sums= 0
+            count = 0
+            if k<0:return 0
+            for r in range(len(nums)):
+                sums += nums[r]
+                while sums> k:
+                    sums-=nums[l]
+                    l+=1
+                count += r-l+1
+            return count
+        ans = notgoal(nums,k) - notgoal(nums,k-1)
+        return ans
+                    
         
