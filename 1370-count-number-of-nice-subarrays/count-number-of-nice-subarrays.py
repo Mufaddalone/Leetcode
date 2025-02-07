1ec952c91d93814ad3c40e35b967743fe148ebcb
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-07 09:36:03 -0800

    Time: 75 ms (80.77%) | Memory: 24.5 MB (28.83%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,16 +1,15 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def notgoal(k):
-            if k<0:return 0
-            l=sums=count=0
-            for r in range(len(nums)):
-                sums += nums[r]%2
-                while sums>k:
-                    sums-=nums[l]%2
-                    l+=1
-                count += r-l+1
-            return count
-        ans = notgoal(k) - notgoal(k-1)
-        return ans
-        
-                
+        res,sums =0,0
+        d= {0:1}
+        for i in nums:
+            if i%2 ==1:
+                sums+=1
+            rem = sums - k
+            if rem in d:
+                res+=d[rem]
+            if sums in d:
+                d[sums] +=1
+            else:
+                d[sums] = 1
+        return res
