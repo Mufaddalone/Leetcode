7807d48f865dac8cb2954bd90fb1bd6a4dcc0fc9
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-09 11:22:10 -0800

    Time: 7 ms (81.13%) | Memory: 17.9 MB (89.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,15 +3,14 @@ class Solution:
         l,r = 0, len(s)-1
         while l<r:
             while l<r and not s[l].isalnum():
-                l+=1 
-            while l<r and not s[r].isalnum():
+                l+=1
+            while r>l and not s[r].isalnum():
                 r-=1
-            if s[l].lower() != s[r].lower():
+            if s[l].lower()!= s[r].lower():
                 return False
-            r,l = r-1, l+1
+            r-=1
+            l+=1
         return True
-
-
  
     # def alphaNum(self, c):
     #     return (ord("A") <= ord(c) <= ord("Z")or 
