7e1772db8b07d058c7d52576d138d8501a02f769
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-09 09:59:19 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.10%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,13 +6,11 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans =[]
+        ans = []
         def dfs(node):
-            if not node:
-                return
+            if not node:return
             dfs(node.left)
             ans.append(node.val)
             dfs(node.right)
         dfs(root)
         return ans
-        
