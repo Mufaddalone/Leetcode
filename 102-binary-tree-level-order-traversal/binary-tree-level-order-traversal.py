7ceb0a66cf640570c4381c887ffb694251af0cf5
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-09 10:11:51 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (92.89%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -8,7 +8,7 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        q =  deque([root])
+        q= deque([root])
         res = []
         while q:
             a = []
@@ -21,63 +21,3 @@ class Solution:
                     q.append(node.right)
             res.append(a)
         return res
-                
-
-        # res = []
-        # def dfs(node,a):
-        #     if not node: return 
-        #     a.append(node.val)
-        #     if node.left:
-        #         dfs(node.left,a)
-        #     if node.right:
-        #         dfs(node.left,a)
-        #     res.append(a)
-        #     return res
-        # return dfs(root,[])
-        
-        # if not root:
-        #     return []
-        # q = deque([root])
-        # res = []
-        # while q:
-        #     val = []
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         val.append(node.val)
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        #     res.append(val)
-        # return res
-
-        
-        # if not root:
-        #     return []
-        # q = deque([root])
-        # a = [[root.val]]
-
-        # while q:
-        #     b = []
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if node.left and node.right:
-        #             b.append(node.left.val)
-        #             b.append(node.right.val)
-        #             q.append(node.left)
-        #             q.append(node.right)
-        #         elif node.left:
-        #             b.append(node.left.val)
-        #             q.append(node.left)
-        #         elif node.right:
-        #             b.append(node.right.val)
-        #             q.append(node.right)
-        #     a.append(b)
-        # a.pop()
-        # return a
-
-
-            
-
-
-        
