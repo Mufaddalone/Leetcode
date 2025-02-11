e01c670eb69bf3cf33aafb1f44feee1d5350aca1
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-11 12:18:45 -0800

    Time: 409 ms (36.55%) | Memory: 17.9 MB (37.41%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res = []
         nums.sort()
+        ans = []
         for i in range(len(nums)-3):
-            if i > 0 and nums[i] == nums[i - 1]:
-                    continue
+            if i>0 and nums[i] == nums[i-1]:
+                continue
             for j in range(i+1,len(nums)-2):
-                if j > i+1 and nums[j] == nums[j - 1]:
+                if j>i+1 and nums[j] == nums[j-1]:
                     continue
                 l,r = j+1,len(nums)-1
                 while l<r:
-                    four = nums[l]+nums[r]+nums[i]+nums[j]
-                    if four<target:
-                        l+=1
-                    elif four>target:
-                        r-=1
-                    else:
-                        res.append([nums[l],nums[r],nums[i],nums[j]])
+                    sums = nums[l]+nums[r]+nums[j]+nums[i]
+                    if sums == target:
+                        ans.append([nums[l],nums[r],nums[j],nums[i]])
                         l+=1
                         r-=1
-                        while l<r and nums[l] ==nums[l-1]:
+                        while l<r and nums[l] == nums[l-1]:
                             l+=1
-                        while r>l and  nums[r] == nums[r+1]:
+                        while l<r and nums[r] == nums[r+1]:
                             r-=1
-        return res
+                    elif sums<target:
+                        l+=1
+                    else:
+                        r-=1
+        return ans
