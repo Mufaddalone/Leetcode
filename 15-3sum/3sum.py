c6b17f662a89bb8f3d66bd8901a816620b190aff
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-11 11:28:04 -0800

    Time: 592 ms (50.85%) | Memory: 20.8 MB (37.78%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,34 +1,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        ans = []
         nums.sort()
-
-        for i,a in enumerate(nums):
-            if i>0 and nums[i-1] == a:
+        for i in range(len(nums)-1):
+            if i > 0 and nums[i] == nums[i - 1]:  
                 continue
-            l,r = i+1, len(nums)-1
+            l,r=i+1,len(nums)-1
             while l<r:
-                sums = nums[l]+nums[r]+a
-                if sums<0:
+                sums =nums[i] + nums[l] + nums[r]
+                if sums == 0:
+                    ans.append([nums[i],nums[l],nums[r]])
                     l+=1
+                    r-=1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
                 elif sums>0:
                     r-=1
                 else:
-                    res.append([nums[l],nums[r],a])
                     l+=1
-                    while l<r and nums[l] == nums[l-1]:
-                        l+=1
-        return res
-
-        # res = []
-        # x= 0
-        # for k in range(1,len(nums)-1):
-        #     l = k+1
-        #     if nums[x] + nums[k]+ nums[l] == 0 and x!=l!=k:
-        #         if [nums[x],nums[k],nums[l]] in res:
-        #             continue
-        #         res.append([nums[x], nums[k], nums[l]])
-        # return res
-                    
-
-        
+        return ans
