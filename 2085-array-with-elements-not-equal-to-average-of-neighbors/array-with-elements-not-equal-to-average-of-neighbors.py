9f031203597a3f3f0575b7a4ea12c1ebf1996d00
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-12 12:35:16 -0800

    Time: 143 ms (43.37%) | Memory: 32.9 MB (26.17%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,15 +1,12 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         nums.sort()
-        res = [0] * len(nums)
-        left, right = 0, (len(nums) + 1) // 2  # Two pointers for interleaving
-
-        for i in range(len(nums)):
-            if i % 2 == 0:
-                res[i] = nums[left]
-                left += 1
-            else:
-                res[i] = nums[right]
-                right += 1
-
+        l,r = 0, len(nums)-1
+        res = []
+        while len(res)!=len(nums) :
+            res.append(nums[l])
+            l+=1
+            if l<=r:
+                res.append(nums[r])
+                r-=1
         return res
