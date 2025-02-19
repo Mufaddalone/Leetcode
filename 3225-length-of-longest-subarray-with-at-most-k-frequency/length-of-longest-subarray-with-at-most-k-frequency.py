e70114b4f8fdddc77b6ec4df66331ee7957389de
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-19 08:42:22 -0800

    Time: 300 ms (55.57%) | Memory: 33.2 MB (27.43%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -3,13 +3,9 @@ class Solution:
         z = defaultdict(int)
         l = 0
         maxlen = 0
-        maxFreq = 0 
         for r in range(len(nums)):
             z[nums[r]]+=1
-            maxFreq = max(maxFreq, z[nums[r]])
-            while maxFreq>k:
-                if z[nums[l]] == maxFreq:
-                    maxFreq -=1
+            while z[nums[r]]>k:
                 z[nums[l]]-=1
                 l+=1
             maxlen = max(maxlen,r-l+1)
