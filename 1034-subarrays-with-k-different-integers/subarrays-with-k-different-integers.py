9764dbb830582091877f09b732536abad3b7b51d
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-02-21 07:48:12 -0800

    Time: 119 ms (72.36%) | Memory: 19.7 MB (44.12%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,18 +1,17 @@
 class Solution:
     def subarraysWithKDistinct(self, s: List[int], k: int) -> int:
-        def atmostk(k):
-            l=0
-            z = defaultdict(int)
+        def atmost(k):
+            l = 0 
+            z = {}
             count = 0
             for r in range(len(s)):
-                z[s[r]]+=1
+                z[s[r]] = z.get(s[r],0)+1
                 while len(z)>k:
-                    z[s[l]]-=1
-                    if z[s[l]] == 0:
+                    z[s[l]] = z.get(s[l],0)-1
+                    if z[s[l]] ==0:
                         del z[s[l]]
                     l+=1
-                count+=r-l+1
+                count+= r-l+1
             return count
-        ans = atmostk(k) - atmostk(k-1)
+        ans = atmost(k) - atmost(k-1)
         return ans
-                        
