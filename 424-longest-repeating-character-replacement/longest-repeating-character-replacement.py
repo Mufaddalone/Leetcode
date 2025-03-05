da730fee5ba2bd39d98337cad9c2f614c2b0ad91
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-03-05 05:27:33 -0800

    Time: 156 ms (40.76%) | Memory: 18.2 MB (23.37%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,12 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l = 0 
-        maxlen = 0
+        l = 0
+        maxi = 0
         z = defaultdict(int)
         for r in range(len(s)):
             z[s[r]]+=1
-            if r-l+1 - max(z.values())>k:
+            if r-l+1-max(z.values())>k:
                 z[s[l]]-=1
                 l+=1
-            maxlen = max(maxlen,r-l+1)
-        return maxlen
+            maxi = max(maxi,r-l+1)
+        return maxi
