1fc5490ed844d440862d5abbf9d0a469135e49d2
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-03-05 05:43:11 -0800

    Time: 3 ms (21.72%) | Memory: 18 MB (32.44%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         closing = {"]":"[","}":"{",")":"("}
-        a = []
-
+        stack = []
         for i in s:
             if i in closing:
-                if a and closing[i]==a[-1]:
-                    a.pop()
+                if stack and stack[-1] == closing[i]:
+                    stack.pop()
                 else:
                     return False
             else:
-                a.append(i)
-        return True if not a else False
-                
+                stack.append(i)
+        return True if not stack else False
+
+
 
 
                 
