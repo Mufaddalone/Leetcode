5949d8e789ad657c958f3dddde90636dbba50f90
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-03-05 06:59:33 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (67.48%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,15 +5,15 @@ class Solution:
             mid = (l+r)//2
             if nums[mid] == target:
                 return mid
-            
-            if nums[mid]>= nums[l]:
-                if nums[l]<= target<=nums[mid]:
-                    r = mid-1
+
+            if nums[mid] >= nums[l]:
+                if nums[l]<=target<=nums[mid]:
+                    r= mid-1
                 else:
-                    l= mid+1
+                    l = mid+1
             else:
-                if nums[mid]<=target<=nums[r]:
-                    l =mid+1
+                if nums[mid]<= target<= nums[r]:
+                    l = mid+1
                 else:
                     r = mid-1
         return -1
