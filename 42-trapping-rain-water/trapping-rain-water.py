d09bd35bdd19a1c0ee37795f40a22ea901a2a76f
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-03-04 20:04:33 -0800

    Time: 11 ms (74.82%) | Memory: 19.3 MB (70.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,20 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # prefix = [height[0]]
-        # for i in range(1,len(height)):
-        #     prefix.append(max(prefix[i-1],height[i]))
-        sufix = [0]*len(height)
-        sufix[-1] = height[-1]
-        for i in range(len(height)-2,-1,-1):
-            sufix[i] = max(sufix[i+1],height[i])
-        total = 0
-        leftmax = height[0]
-        for i in range(len(height)):
-            if height[i]<=leftmax and height[i]<= sufix[i]:
-                total += min(leftmax,sufix[i]) - height[i]
-            leftmax = max(leftmax,height[i])
-        return total
-        # for i in range(len(height)):
-        #     if height[i] <= prefix[i] and height[i] <= sufix[i]:
-        #         total += min(prefix[i],sufix[i]) - height[i]
-        # return total
+        l = 0
+        res = 0
+        r = len(height)-1
+        leftmax,rightmax = height[l],height[r]
+        while l<r:
+            if leftmax<rightmax:
+                l+=1
+                leftmax = max(leftmax,height[l])
+                res += leftmax - height[l]
+            else:
+                r-=1
+                rightmax = max(rightmax,height[r])
+                res += rightmax - height[r]
+        return res
+
