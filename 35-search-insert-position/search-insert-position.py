e482d4b67c53fb9d553fef33e599e486695143cc
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2025-03-10 17:20:26 -0700

    Time: 0 ms (100.00%) | Memory: 18.5 MB (44.93%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,8 +7,7 @@ class Solution:
             if nums[mid] == target:
                 return mid
             if nums[mid] < target:
-                ans = mid
-                l+=1
+                l = mid+1
             else:
-                r-=1
-        return ans+1
+                r = mid-1
+        return l
