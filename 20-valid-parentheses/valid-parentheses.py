dc49d60d61dda86f521482fa90deadb559a846ba
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2026-01-15 23:35:52 -0800

    Time: 3 ms (36.95%) | Memory: 19.4 MB (5.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,13 +3,11 @@ class Solution:
         closing = {"}":"{","]":"[",")":"("}
         a = []
         for i in s:
-            if i in closing and len(a)!=0:
-                if a[-1] == closing[i]:
+            if i in closing:
+                if a and a[-1] == closing[i]:
                     a.pop()
                 else:
                     return False
-            elif i in closing:
-                return False
             else:
                 a.append(i)
         return len(a)==0
