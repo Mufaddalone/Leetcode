111c9fff8f6fe9743462d98c2d4d56846f047c43
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2026-01-16 22:44:14 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (13.89%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,7 +10,7 @@ class Solution:
         while l1 and l2:
             if l1.val<l2.val:
                 tail.next = l1
-                l1 = l1.next
+                l1= l1.next
             else:
                 tail.next = l2
                 l2 = l2.next
@@ -20,4 +20,3 @@ class Solution:
         if l2:
             tail.next = l2
         return dummy.next
-        
