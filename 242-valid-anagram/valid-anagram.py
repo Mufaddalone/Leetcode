1c1bdf7aef2121bbd03ace3f016233ff397b0d4e
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2026-03-16 00:02:11 -0700

    Time: 9 ms (79.56%) | Memory: 19.3 MB (90.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        a = {}
+        a = defaultdict(int)
         for i in s:
-            a[i] = a.get(i,0)+1
+            a[i]+=1
         for j in t:
-            a[j] = a.get(j,0)-1
-        
+            a[j]-=1
         for i in a.values():
             if i!=0:
                 return False
         return True
+        
